14fe2815694db9ac37f08e3595555850388b988e
Francis Adegbe <Francismission61@mail.com>
2022-06-30 08:28:09 -0700

    C programming task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: always 0
- *
- *     */
-
+*main - Entry point
+*
+*My hello, printf task
+*
+*Return: return 1 after program execution
+*/
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
