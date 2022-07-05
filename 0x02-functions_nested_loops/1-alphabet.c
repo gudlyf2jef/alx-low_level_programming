c6ace4c4c5d143781a4652ac7d80932027f62a06
Francis Adegbe <Francismission61@mail.com>
2022-07-05 08:46:29 -0700

    a function that prints alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- *main - print the lower case  alphabet
- *Return: always return 0
- */
+*print_alphabet  -> print the lower case  alphabet
+*
+*Return: always return 0
+*/
 void print_alphabet(void)
 {
 int j;
