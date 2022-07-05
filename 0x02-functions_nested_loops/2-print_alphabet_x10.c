77c7e5eed3ffd7be34f41a56524f98fa34c111b6
Francis Adegbe <Francismission61@mail.com>
2022-07-05 11:29:01 -0700

    task for nested loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
 * print_alphabet_x10 -> prints the lowercase alphabet
+*
+*Retunr: always returns 0
 */
 void print_alphabet_10x(void)
 {
-int j;
-int x;
-for (x = 0; x < 10; x++)
+char j;
+char x;
+for (x = 0; x <= 9; x++)
 {
 for (j = 'a'; j <= 'z'; j++)
 {
