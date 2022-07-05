7bfc9cf570a8b4b3ae7d3ec0e657a48421c6954f
Francis Adegbe <Francismission61@mail.com>
2022-07-05 08:57:11 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_alphabet_x10 -> a function that proints 10 letters
+*
+* Return: always returns 0
+*/
+void print_alphabet_10x(void);
+{
+int j,x;
+for (x = 0; x < 10; x++)
+{
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
+}
