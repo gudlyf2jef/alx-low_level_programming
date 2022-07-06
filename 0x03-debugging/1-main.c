e74939c8ffb85f15f31fb88552fff7445572de83
Francis Adegbe <Francismission61@mail.com>
2022-07-06 02:52:26 -0700

    commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
 * main - causes an infinite loop
+*@i: while argument variable
 * Return: 0
 */
 int main(void)
@@ -8,6 +9,12 @@ int main(void)
 int i;
 printf("Infinite loop incoming \n");
 i = 0;
+/**
+* while (i < 10)
+*{
+* putchar(i);
+*}
+*/
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
