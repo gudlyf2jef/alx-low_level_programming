9ba9fb209d16759463ca03cd818b326547cbaad8
Francis Adegbe <Francismission61@mail.com>
2022-07-12 11:13:05 -0700

    pointer task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <string.h>
-
 /**
 *_strlen -> function to get the length of a string
 *@s: string pointer to pass to this function
@@ -10,7 +9,7 @@ int _strlen(char *s)
 {
 	int len;
 
-	for(; *s != '\0'; s++)
+	for (; *s != '\0'; s++)
 	{
 		len += 1;
 	}
