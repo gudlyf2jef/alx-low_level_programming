81c25cb55573c5b3504605963084b190f5e4ea3f
Francis Adegbe <Francismission61@mail.com>
2022-07-12 15:19:12 -0700

    pointer tasks

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include "main.h"
+#include <limits.h>
+
+/**
+* _atoi - coverts a string to integer
+* @str: pointer to string to be converted
+* Return: converted string
+*/
+
+
+
+int _atoi(char *str)
+{
+int sign = 1, base = 0, i = 0;
+while (str[i] == ' ')
+{
+i++;
+}
+if (str[i] == '-' || str[i] == '+')
+{
+sign = 1 - 2 * (str[i++] == '-');
+}
+while (str[i] >= '0' && str[i] <= '9')
+{
+if (base > INT_MAX / 10	|| (base == INT_MAX / 10 && str[i] - '0' > 7))
+{
+if (sign == 1)
+return (INT_MAX);
+else
+return (INT_MIN);
+}
+base = 10 * base + (str[i++] - '0');
+}
+	return (base *sign);
+}
