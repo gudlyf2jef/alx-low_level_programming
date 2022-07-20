4b9fc0931aea439b750e5a1b7975389911a84ce3
Francis Adegbe <Francismission61@mail.com>
2022-07-20 15:16:55 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#inlude "main.h"
+
+/**
+ * _puts_recursion - a function that prints a string
+ * @s: input
+ * Return: always 0
+ */
+voide _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_rcursion(s + 1);
+	}
+}
